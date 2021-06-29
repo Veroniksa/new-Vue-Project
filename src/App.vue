2f67a1384ecc3bd5f11309d8e1f0741878c1049d
Veronika <desatericenkoveronika@gmail.com>
2021-06-29 10:16:12 +0200

    les1

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Calc />
+    <HelloWorld msg="Другие данные" :myArr="arr"
+    />
+    Text : {{ message }}  длинна {{message.length}}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Calc
+  },
+  data: () => ({
+    message: 'Реклама',
+    arr: [1, 2, 4, 5]
+  })
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calc.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <h1>{{ title }}</h1>
+    <div>
+        <input type="number" placeholder="op1" v-model.number="operand1">
+        <input type="number" placeholder="op2" v-model.number="operand2">
+        = {{ sum }}
+    </div>
+      <div>
+        <button @click="sum = operand1 + operand2">+</button>
+        <button v-on:click="sum = operand1 - operand2">-</button>
+        <button @click="sum = operand1 * operand2">*</button>
+        <button @click="div">/</button>
+        <button v-on:click="expo">^</button>
+        <button v-on:click="int">%</button>
+        <button @click="eventFn">Event</button>
+      </div>
+    Result {{ sum }}
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Calc',
+  data: () => ({
+    operand1: 0,
+    operand2: 0,
+    sum: 0,
+    title: 'Калькулятор'
+  }),
+  methods: {
+    eventFn () {
+      console.log(arguments)
+    },
+    div () {
+      this.sum = this.operand1 / this.operand2
+    },
+    expo () {
+      this.sum = Math.pow(this.operand1, this.operand2)
+    },
+    int () {
+      if (this.operand1 < this.operand2) {
+        this.sum = Math.floor(this.operand1 / this.operand2)
+      } else if (this.operand1 > this.operand2) {
+        this.sum = Math.ceil(this.operand1 / this.operand2)
+      } else if (this.operand1 || this.operand2 === 0) {
+        this.sum = null
+      }
+    }
+  }
+}
+
+</script>
